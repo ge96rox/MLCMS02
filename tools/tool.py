dc73f3e5ae9431b78e3e3cdfd78cd50e57b01865
Yinnan Wu <Yinnan.Wu@tum.de>
2021-05-12 12:15:21 +0200

    add comment

--- tools/tool.py
+++ tools/tool.py
@@ -3,16 +3,44 @@ import os
 
 
 class ScenarioTool:
+    """
+       A class represent JSON editing tools
+       Parameters
+       ----------
+       scenario_filename : str
+           The scenario filename for editing
+       Attributes
+       ----------
+       scenario_filename : str
+           The scenario filename for editing
+       scenario_json : dict
+           The dict storing content of scenario file
+       pedestrian_json: dict
+           The dict storing the pedestrian information
+    """
     def __init__(self, scenario_filename):
         self.scenario_filename = scenario_filename
         self.scenario_json = None
         self.pedestrian_json = None
 
     def load(self):
+        """
+        load the scenario
+        """
         with open(self.scenario_filename) as scenario_fd:
             self.scenario_json = json.load(scenario_fd)
 
     def add_ped(self, ped_filename):
+        """
+        add pedestrian information to scenario files
+        Parameters
+        ----------
+        ped_filename : str
+            The pedestrian JSON
+        Returns
+        -------
+        None
+        """
         with open(ped_filename) as ped_fd:
             file_basename= os.path.basename(self.scenario_filename)
             self.scenario_json["name"] = file_basename.split('.')[0] + "_modify"
@@ -22,6 +50,12 @@ class ScenarioTool:
             self.scenario_json["scenario"]["topography"]["dynamicElements"].append(pedestrian_json)
 
     def save(self, save_filename):
+        """
+        save the modified scenario file
+        Returns
+        -------
+        None
+        """
         with open(save_filename, 'w') as save_fd:
             json.dump(self.scenario_json, save_fd)
 
